608043c25796cfc790289473fe03ed311287a8ae
Can Pekesen <can@pekesen.de>
2016-10-07 09:41:33 +0200

    fix(mocha): lint config

--- test-mocha.js
+++ test-mocha.js
@@ -1,16 +1,16 @@
 module.exports = {
-  "extends": "rea",
   "plugins": [
     "mocha",
   ],
+  "extends": "rea",
+  "env": {
+    "mocha": true,
+  },
   "rules": {
     "max-nested-callbacks": [2, 15], // them its and describes...
     "max-len": [2, 240], // them verbose it statements
     "mocha/no-exclusive-tests": 2, // never push exclusive tests
     "mocha/handle-done-callback": 2, // don't forget that "done" !
-  },
-  "env": {
-    "mocha": true,
   },
   "globals": {
     "describe": false,
