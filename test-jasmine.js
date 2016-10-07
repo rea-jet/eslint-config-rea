0ac4880d68787ac6095c23e8a27d3fe365973d8e
Can Pekesen <can@pekesen.de>
2016-10-07 09:39:18 +0200

    fix(jasmine): lint config

--- test-jasmine.js
+++ test-jasmine.js
@@ -1,16 +1,17 @@
 module.exports = {
-  "extends": "rea",
   "plugins": [
-    "mocha",
+    "jasmine",
   ],
+  "extends": [
+    "rea",
+    "plugin:jasmine/recommended",
+  ],
+  "env": {
+    "jasmine": true,
+  },
   "rules": {
     "max-nested-callbacks": [2, 15], // them its and describes...
     "max-len": [2, 240], // them verbose it statements
-    "mocha/no-exclusive-tests": 2, // never push exclusive tests
-    "mocha/handle-done-callback": 2, // don't forget that "done" !
-  },
-  "env": {
-    "mocha": true,
   },
   "globals": {
     "describe": false,
@@ -20,4 +21,4 @@ module.exports = {
     "expect": false,
     "inject": false,
   },
-}
+};
