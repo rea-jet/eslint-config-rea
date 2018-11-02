d90f0f3118b9a49b5e59fe186820cb6c85a1a0d2
Dennis Saenger <dsaenger@rea.de>
2018-11-02 09:51:29 +0100

    feat: lint local rules with itself

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports = {
   },
   parserOptions: {
     ecmaVersion: 8,
-    sourceType: "module",
+    sourceType: 'module',
     ecmaFeatures: {
       modules: true
     }
@@ -20,30 +20,30 @@ module.exports = {
     angular: false
   },
   rules: {
-    curly: [2, "all"],
+    curly: [2, 'all'],
     indent: [2, 2, {SwitchCase: 1}],
-    quotes: [2, "single"],
-    "max-params": [2, 4],
-    "max-depth": [2, 3],
-    "max-statements": [2, 20],
-    "max-len": [2, 80, 2, {ignoreComments: true, ignoreUrls: true}],
-    "no-undef": 2,
-    "no-unused-vars": 2,
-    "operator-linebreak": [2, "after"],
-    "no-multi-str": 2,
-    "no-mixed-spaces-and-tabs": 2,
-    "no-trailing-spaces": 2,
-    "space-unary-ops": 2,
-    "one-var": [2, {uninitialized: "always", initialized: "never"}],
-    "brace-style": [2, "1tbs", {allowSingleLine: true}],
-    "keyword-spacing": 2,
-    "space-infix-ops": 2,
-    "space-before-blocks": [2, "always"],
-    "eol-last": [2, "always"],
-    "array-bracket-spacing": [2, "never"],
-    "object-curly-spacing": [2, "never"],
-    "space-in-parens": [2, "never"],
-    "no-multiple-empty-lines": 2,
-    "semi-spacing": [2, {before: false, after: true}]
+    quotes: [2, 'single'],
+    'max-params': [2, 4],
+    'max-depth': [2, 3],
+    'max-statements': [2, 20],
+    'max-len': [2, 80, 2, {ignoreComments: true, ignoreUrls: true}],
+    'no-undef': 2,
+    'no-unused-vars': 2,
+    'operator-linebreak': [2, 'after'],
+    'no-multi-str': 2,
+    'no-mixed-spaces-and-tabs': 2,
+    'no-trailing-spaces': 2,
+    'space-unary-ops': 2,
+    'one-var': [2, {uninitialized: 'always', initialized: 'never'}],
+    'brace-style': [2, '1tbs', {allowSingleLine: true}],
+    'keyword-spacing': 2,
+    'space-infix-ops': 2,
+    'space-before-blocks': [2, 'always'],
+    'eol-last': [2, 'always'],
+    'array-bracket-spacing': [2, 'never'],
+    'object-curly-spacing': [2, 'never'],
+    'space-in-parens': [2, 'never'],
+    'no-multiple-empty-lines': 2,
+    'semi-spacing': [2, {before: false, after: true}]
   }
 };
