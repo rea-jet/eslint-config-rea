9596607329a9b2f9f755337e2cd4507227449fc6
Dennis Saenger <dsaenger@rea.de>
2019-11-28 13:04:50 +0100

    feat: object-curly-spacing: never -> always

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ module.exports = {
   },
   rules: {
     curly: [2, 'all'],
-    indent: [2, 2, {SwitchCase: 1}],
+    indent: [2, 2, { SwitchCase: 1 }],
     quotes: [2, 'single'],
     'max-params': [2, 4],
     'max-depth': [2, 3],
@@ -45,16 +45,16 @@ module.exports = {
     'no-mixed-spaces-and-tabs': 2,
     'no-trailing-spaces': 2,
     'space-unary-ops': 2,
-    'one-var': [2, {uninitialized: 'always', initialized: 'never'}],
-    'brace-style': [2, '1tbs', {allowSingleLine: true}],
+    'one-var': [2, { uninitialized: 'always', initialized: 'never' }],
+    'brace-style': [2, '1tbs', { allowSingleLine: true }],
     'keyword-spacing': 2,
     'space-infix-ops': 2,
     'space-before-blocks': [2, 'always'],
     'eol-last': [2, 'always'],
     'array-bracket-spacing': [2, 'never'],
-    'object-curly-spacing': [2, 'never'],
+    'object-curly-spacing': [2, 'always'],
     'space-in-parens': [2, 'never'],
     'no-multiple-empty-lines': 2,
-    'semi-spacing': [2, {before: false, after: true}]
+    'semi-spacing': [2, { before: false, after: true }]
   }
 };
