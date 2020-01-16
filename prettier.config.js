fc00fcea78b2ecbf54d59b1959c56537c3f18309
Dennis Saenger <dsaenger@rea.de>
2020-01-16 08:28:14 +0100

    chore(prettier): add matching prettier config

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  trailingComma: 'es5',
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  printWidth: 80,
+  jsxSingleQuote: false,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  endOfLine: 'lf',
+};
